044e9ef903cb43bd0852d98a3b98245a300b6684
Marketgai <marcusalee@gmail.com>
2020-12-07 21:52:21 -0800

    seed.sh working with bash

--- arango/seed.sh
+++ arango/seed.sh
@@ -1,2 +1,12 @@
-arangoimport --file "./db/" --type csv --collection "listings" --server.database "stayio"
+arangoimport --file "/Users/marcuslee/Desktop/HR/SDC/service/db/csv/graph/graphContainsEdge.csv" --type csv --collection "contains" --server.database "testing";
 
+arangoimport --file "/Users/marcuslee/Desktop/HR/SDC/service/db/csv/graph/graphFavList.csv" --type csv --collection "favList" --server.database "testing";
+
+arangoimport --file "/Users/marcuslee/Desktop/HR/SDC/service/db/csv/graph/graphListing.csv" --type csv --collection "listings" --server.database "testing";
+
+arangoimport --file "/Users/marcuslee/Desktop/HR/SDC/service/db/csv/graph/graphOwnsEdge.csv" --type csv --collection "owns" --server.database "testing";
+
+arangoimport --file "/Users/marcuslee/Desktop/HR/SDC/service/db/csv/graph/graphUser.csv" --type csv --collection "user" --server.database "testing";
+
+
+# run with bash pathname
